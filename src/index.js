cd3c6074299db6ce475dc82a568f9761fff2c55b
Luiz Ramos <luizgdramos@gmail.com>
2019-01-25 13:54:42 +0000

    Desafio avançado da aula 022

--- src/index.js
+++ src/index.js
@@ -1,57 +1,43 @@
 const $root = document.querySelector('#root');
-const $memoryCard = document.createElement('article');
-const $memoryCard1 = document.createElement('article');
-const $memoryCard2 = document.createElement('article');
-const $memoryCard3 = document.createElement('article');
-const $memoryCard4 = document.createElement('article');
-const $memoryCard5 = document.createElement('article');
-const $memoryCard6 = document.createElement('article');
-const $memoryCardFront = document.createElement('article');
 
-const $iconCollab = `
-    <img 
-        src="img/icon-collabcode.png" 
-        alt="Gueio mascote da CollabCode" 
-        class="icon"
-    />
-`;
+for (let i = 0; i < 7; i++) {
+    const $memoryCard = document.createElement('article');
+    const $memoryCardFront = document.createElement('article');
+    
+    createMemoryCardElement($memoryCard);
+    if (i == 0) {
+        createMemoryCardFrontElement($memoryCardFront, $memoryCard);
+    }
+}
 
-const $iconC = `
-    <img 
-        src="img/icon-c.png" 
-        alt="Linguagem C++" 
-        class="icon"
-    />
-`;
+function createMemoryCardElement($memoryCard) {
 
-$memoryCard.classList.add('memory-card');
-$root.insertBefore($memoryCard, null);
-$memoryCard.insertAdjacentHTML('afterbegin', $iconCollab);
+    const $iconCollab = `
+        <img 
+            src="img/icon-collabcode.png" 
+            alt="Gueio mascote da CollabCode" 
+            class="icon"
+        />
+    `;
 
-$memoryCardFront.classList.add('memory-card', '-front');
-$root.insertBefore($memoryCardFront, $memoryCard);
-$memoryCardFront.insertAdjacentHTML('afterbegin', $iconC);
+    $memoryCard.classList.add('memory-card');
+    $root.insertBefore($memoryCard, null);
+    $memoryCard.insertAdjacentHTML('afterbegin', $iconCollab);
 
-$memoryCard1.classList.add('memory-card');
-$root.insertBefore($memoryCard1, null);
-$memoryCard1.insertAdjacentHTML('afterbegin', $iconCollab);
+    return $memoryCard;
+}
 
-$memoryCard2.classList.add('memory-card');
-$root.insertBefore($memoryCard2, null);
-$memoryCard2.insertAdjacentHTML('afterbegin', $iconCollab);
+function createMemoryCardFrontElement($memoryCardFront, $memoryCard) {
 
-$memoryCard3.classList.add('memory-card');
-$root.insertBefore($memoryCard3, null);
-$memoryCard3.insertAdjacentHTML('afterbegin', $iconCollab);
+    const $iconC = `
+        <img 
+            src="img/icon-c.png" 
+            alt="Linguagem C++" 
+            class="icon"
+        />
+    `;
 
-$memoryCard4.classList.add('memory-card');
-$root.insertBefore($memoryCard4, null);
-$memoryCard4.insertAdjacentHTML('afterbegin', $iconCollab);
-
-$memoryCard5.classList.add('memory-card');
-$root.insertBefore($memoryCard5, null);
-$memoryCard5.insertAdjacentHTML('afterbegin', $iconCollab);
-
-$memoryCard6.classList.add('memory-card');
-$root.insertBefore($memoryCard6, null);
-$memoryCard6.insertAdjacentHTML('afterbegin', $iconCollab);
+    $memoryCardFront.classList.add('memory-card', '-front');
+    $root.insertBefore($memoryCardFront, $memoryCard);
+    $memoryCardFront.insertAdjacentHTML('afterbegin', $iconC);
+}
