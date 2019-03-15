b46e2f961e5f74b998a6bd779baca900c6f40d12
LuizRamos19 <luizgdramos@gmail.com>
2019-03-14 22:21:49 -0300

    Corrigindo desafios básico, intermediário e avançado da aula 038

--- src/components/flat-button/index.js
+++ src/components/flat-button/index.js
@@ -1,35 +1,33 @@
 const flatButton = (function() {
     const module = {};
 
-    module._style = () => {
+    module._id = 0;
+
+    module._style = (active) => {
         const $head = document.querySelector("head");
         const $style = document.createElement("style");
 
         $style.textContent = `
-            .flat-button {
-                background-color: #eae6da;
-                color: #fffcee;
+            .flat-button-${module._id} {
+                background-color: ${active ? '#f25a70' : '#eae6da'};
+                color: ${active ? '#fff' : '#fffcee'};
                 font-size: 24px;
                 font-weight: bold;
-                width: 186px;
+                width: 50%;
                 height: 176px;
                 text-transform: uppercase;
-            }
-            .-sigup {
-                background-color: #f25a70;
-                position: absolute;
-                right: 0;
             }
         `;
 
         $head.insertAdjacentElement('beforeend', $style);
     };
 
-    module.render = obj => {
-        module._style();
+    module.render = (content = "", active = false) => {
+        module._id++;
+        module._style(active);
 
         return `
-            <button class="${obj.classButton}">${obj.className}</button>
+            <button class="flat-button-${module._id}">${content}</button>
         `;
     };
 
--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,8 +1,8 @@
 (function(){
     const $root = document.querySelector('#root');
     
-    const $flatButtonLogin = flatButton.render({classButton: 'flat-button', className: 'Login'});
-    const $flatButtonSigup = flatButton.render({classButton: 'flat-button -sigup', className: 'Sig up'});
+    const $flatButtonLogin = flatButton.render('Login');
+    const $flatButtonSigup = flatButton.render('Sign up', true);
 
     $root.insertAdjacentHTML('beforeend', $flatButtonLogin);
     $root.insertAdjacentHTML('beforeend', $flatButtonSigup);
